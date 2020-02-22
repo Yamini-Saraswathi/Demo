6c2d3d5092da76eddd78922981362ecb64692491
Yamini-Saraswathi <54294464+Yamini-Saraswathi@users.noreply.github.com>
2020-02-22 16:15:56 -0600

    Adding ToDo App

---
+++ app/core/version/interpolate-filter.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+
+angular.module('myApp.version.interpolate-filter', [])
+
+
+
+  .filter('interpolate', ['version', function(version) {
+
+    return function(text) {
+
+      return String(text).replace(/\%VERSION\%/mg, version);
+
+    };
+
+  }]);
---
+++ app/core/version/version-directive.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+
+angular.module('myApp.version.version-directive', [])
+
+
+
+  .directive('appVersion', ['version', function(version) {
+
+    return function(scope, elm, attrs) {
+
+      elm.text(version);
+
+    };
+
+  }]);
